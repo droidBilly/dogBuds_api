2a706f2d0fb017caf9a0afa4b3ebef1084fe719e
lucadido06 <lucadidonato06@gmail.com>
2018-03-06 14:05:58 +0100

    dog updated

--- user/model.js
+++ user/model.js
@@ -1,4 +1,22 @@
 const Sequelize = require('sequelize')
 const sequelize = require('../db')
 
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  tableName: 'users',
+  timestamps: false
+})
+
+
 module.exports = User
