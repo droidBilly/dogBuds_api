c951b1f83a0d9344e949471d98c415105b06901e
FStriewski <fstriewski@outlook.com>
2018-03-06 14:34:48 +0100

    Project cleanup

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
+const app = express()
+
+app.use(cors())
 app.use(bodyParser.json())
 
 app.listen(4001, () => console.log('Express API listening on port 4001'))
-
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
-  next()
-})
 
 app.get('/', (request, response) => {
       response.send( { message: 'Yay, your setup is workig! Happy coding! BARK! BARK!' })
