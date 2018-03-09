8caf5bd63dc2498fd057b493ab9c4ec3c1393a29
lucadido06 <lucadidonato06@gmail.com>
2018-03-09 15:15:56 +0100

    user no preferences solved

--- users/matchesAlgo.js
+++ users/matchesAlgo.js
@@ -1,51 +1,53 @@
-function compareUsers(ourUser, otherUser){
- if (ourUser !== otherUser) {
-  let matchingDogs =
-      ourUser.info.preferences
-          .map(x => otherUser.info.preferences.filter(y => y.breed == x.breed))
-          .reduce((a,b) => a.concat(b))
-          .map(x => x.breed)
+function compareUsers(ourUser, otherUser) {
+  if (ourUser !== otherUser ) {
+    let matchingDogs = ourUser.info.preferences
+      .map(x => otherUser.info.preferences.filter(y => y.breed == x.breed))
+      .reduce((a, b) => a.concat(b))
+      .map(x => x.breed);
 
-    function calcDogScore(user){
+    function calcDogScore(user) {
+      let sumDogs = (function() {
+        return user.info.preferences.reduce(
+          (sum, breed) => sum + breed.votes,
+          0
+        );
+      })();
 
-        let sumDogs = function (){
-            return user.info.preferences.reduce((sum, breed) => sum + breed.votes , 0)
-        }();
+      function perc(vote) {
+        return Math.floor(vote / sumDogs * 100);
+      }
 
-        function perc(vote){
-            return Math.floor((vote / sumDogs ) * 100)
-        }
-
-        function score(user){
-            return user.info.preferences
-                .filter(x => matchingDogs.indexOf(x.breed) >= 0 )
-                .reduce((sum, x) =>  {return sum + perc(x.votes)},0 )
-        }
-        return score(user)
+      function score(user) {
+        return user.info.preferences
+          .filter(x => matchingDogs.indexOf(x.breed) >= 0)
+          .reduce((sum, x) => {
+            return sum + perc(x.votes);
+          }, 0);
+      }
+      return score(user);
     }
 
-    let sum = (calcDogScore(ourUser) + calcDogScore(otherUser))
-    let mean = sum !== 0 ? (sum/2) : 0
+    let sum = calcDogScore(ourUser) + calcDogScore(otherUser);
+    let mean = sum !== 0 ? sum / 2 : 0;
 
-    let result = []
-      result.push({"Score":  mean})
-      result.push(otherUser.info)
-      return result
-    }
-
-}
-
-
-function compareAllUsers (user1, users) {
-  if (typeof user1 !== "undefined") {
-  let matches = []
-  for (var i = 0; i < users.length; i++) {
-    matches.push(compareUsers(user1, users[i]))
+    let result = [];
+    result.push({ Score: mean });
+    result.push(otherUser.info);
+    return result;
   }
-  return matches
-  .sort((a,b) => a[0].Score - b[0].Score)
-  .slice(0, 10).reverse()
-}
 }
 
-module.exports.compareAllUsers = compareAllUsers  ;
+function compareAllUsers(user1, users) {
+  if (typeof user1 !== "undefined") {
+    let matches = [];
+    for (var i = 0; i < users.length; i++) {
+      matches.push(compareUsers(user1, users[i]));
+    }
+    return matches
+      .sort((a, b) => a[0].Score - b[0].Score)
+      .slice(0, 10)
+      .reverse();
+  }
+}
+
+module.exports.compareAllUsers = compareAllUsers;
